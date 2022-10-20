154a9124e5e36eb9fee2e17a2cf7a768b5ce26a8
teasa7 <nastya.chinyaeva@gmail.com>
2022-10-20 12:45:35 +0500

    first commit

---
+++ autorization.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Авторизация</title>
+        <meta charset="utf-8">
+        <link href="css/autorization.css" type="text/css" rel="stylesheet" media="screen,projection">
+
+        <!-- Шрифты -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jura:wght@300;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <main>
+            <div class="blockOpacity">
+                <table>
+                    <tr>
+                        <td><h1><a href="autorization.php">Вход</a></h1></td>
+                        <td><h1><a href="registration.php">Регистрация</a></h1></td>
+                    </tr>
+                </table>    
+                <form class="formAutorization">
+                    <div class="blockPadding">
+                        <label>Электронная почта/номер телефона</label><br>
+                        <input type="email"><br>
+                        <label>Пароль</label><br>
+                        <input type="password"><br>
+                    </div>
+                    <input type="button" value="Войти">  
+                </form>
+            </div>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
---
+++ registration.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Регистрация</title>
+        <meta charset="utf-8">
+        <link href="css/registration.css" type="text/css" rel="stylesheet" media="screen,projection">
+
+        <!-- Шрифты -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jura:wght@300;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <main>
+            <div class="blockOpacity">
+                <table>
+                    <tr>
+                        <td><h1><a href="autorization.php">Вход</a></h1></td>
+                        <td><h1><a href="registration.php">Регистрация</a></h1></td>
+                    </tr>
+                </table>  
+                <form class="formAutorization">
+                    <div class="blockPadding">
+                        <label>Имя <span class="redtext">*</span></label><br>
+                        <input type="text"><br>
+                        <label>Фамилия<span class="redtext">*</span></label><br>
+                        <input type="text"><br>
+                        <label>Электронная почта<span class="redtext">*</span></label><br>
+                        <input type="email" pattern="^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+"><br>
+                        <label>Номер телефона</label><br>
+                        <input type="tel"><br>
+                        <label>Пароль<span class="redtext">*</span></label></label><br>
+                        <input type="password"><br>
+                        <label>Повтор пароля <span class="redtext">*</span></label></label><br>
+                        <input type="password"><br>
+                        <div class="acceptData">
+                            <input type="checkbox"><a>Принять согласие на обработку персональных данных</a><span class="redtext">*</span></label>
+                        </div>
+                        
+                    </div>
+                    
+                    <input type="button" value="Далее">  
+                </form>
+            </div>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
